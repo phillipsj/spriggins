19e6a8589422ebd960e55f23b343edf644184378
Charles Phillips <cphill11@gmail.com>
2018-05-29 20:57:16 -0400

    Basics getting in place.

--- src/Spriggins/Program.cs
+++ src/Spriggins/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
 
-namespace Spriggins
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Spriggins {
+    public class Program {
+        public static int Main(string[] args) {
+            var app = new CommandLineApplication {
+                FullName = "spriggins",
+                ThrowOnUnexpectedArgument = false
+            };
+            app.HelpOption(inherited: true);
+            app.VersionOption("-v|--version", "0.0.1");
+
+            var fileOption = app.Option("-f|--file <FILE>", "The manifest file to create the package.", CommandOptionType.MultipleValue);
+
+            app.OnExecute(() => {
+                if (fileOption.HasValue()) {
+                    Console.WriteLine("Using the file passed...");
+                }
+                else {
+                    Console.WriteLine("Using the default file...");
+                }
+            });
+            
+            return app.Execute(args);
         }
+        
     }
 }
